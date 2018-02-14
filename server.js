31df5eba04b4943283cf5e3438e0584f25f157ff
shulme801 <shulme801@gmail.com>
2018-02-14 18:03:21 -0500

    14 Feb server.js works

--- server.js
+++ server.js
@@ -1,69 +1,37 @@
 // Dependencies
 // =============================================================
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongojs = require("mongojs");
-// Require request and cheerio. This makes the scraping possible
-const request = require("request");
-const cheerio = require("cheerio");
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
+// Node Dependencies
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-// Routes
-// =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-require("./routes/user-api-routes.js")(app);
+var logger = require('morgan'); // for debugging
+var request = require('request'); // for web-scraping
+var cheerio = require('cheerio'); // for web-scraping
 
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = process.env.PORT || 3000;
+// Initialize Express for debugging and body parsing
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 
-// Allows Express app to handle data parsing
-// =============================================================
+// Serve Static Content
+app.use(express.static(process.cwd() + '/public'));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
+// Express-Handlebars
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// Allows Express app to serve static files in the public folder
-app.use(express.static("public"));
 
-// Set Handlebars.
-const exphbs = require("express-handlebars");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.get("/", function(req, res) {
-  res.render("index");
-});
-
-//Database Configuration with Mongoose
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: false }));
-// Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
-
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-// Connect to localhost if not a production environment
-if (config.use_env_variable === 'development'){
-	mongoose.connect('mongodb://localhost/news-scraper_dev', {
-		useMongoClient = true;
-	});
-else
-  mongoose.connect('MONGODB_URI', {
-  	useMongoClient = true;
-  });
-}
+mongoose.connect(MONGODB_URI);
 
 const db = mongoose.connection;
 
@@ -75,11 +43,14 @@ db.on('error', function(err) {
 // Once logged in to the db through mongoose, log a success message
 db.once('open', function() {
   console.log('Mongoose connection successful.');
+  // Listen either on the port Heroku gives us, or on 8080 if we are running on localhost. 
+  var PORT = process.env.PORT || 8080;
+
+  // Start our app's listening on either port 8080 or the port that heroku gave us
+  app.listen(PORT, function() {
+     console.log("Mongoose news scraper alive and listening on Port "+ PORT);
+  });
 });
 
 
-// Start our app's listening on either port 3000 or the port that heroku gave us
-app.listen(3000, function() {
-  console.log("Mongoose news scraper alive and listening on Port "+ PORT);
-});
 
